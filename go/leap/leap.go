76706059854355cd32ed79ef24aafe256d87e85a
prataprc <prataprc@gmail.com>
2017-04-24 13:41:09 +0530

    go: leap, optimized version.

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,15 +3,5 @@ package leap
 const testVersion = 3
 
 func IsLeapYear(year int) bool {
-	if (year % 4) != 0 {
-		return false
-	}
-	mod := year % 100
-	if mod != 0 {
-		return true
-	}
-	if (year % 400) == 0 {
-		return true
-	}
-	return false
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
